2a94034af61462c9ea40fe8649a07c2a1b31b309
raccoon91 <qsx314@gmail.com>
2020-04-19 00:15:52 +0900

    add axios instance

---
+++ src/lib/requestWeatherServer.ts
@@ -0,0 +1,10 @@
+import axios, { AxiosInstance } from "axios";
+import config from "../config";
+
+const BASE_URL = config.RACCOON_WEATHER_SERVER_URL;
+
+const requestWeatherServer: AxiosInstance = axios.create({
+  baseURL: BASE_URL,
+});
+
+export default requestWeatherServer;
