8d96f82e4839a51043b7f0098850d83b3c505ee5
raccoon91 <dev.beomseok@gmail.com>
2020-04-19 00:21:58 +0900

    add CurrentWeather component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import React, { FC } from "react";
 import styled from "styled-components";
 
+import CurrentWeather from "./components/CurrentWeather";
+
 const AppContainer = styled.div`
-  width: 300px;
-  height: 300px;
+  width: 500px;
+  min-height: 300px;
+  border: 1px solid black;
 `;
 
 const App: FC = () => {
-  return <AppContainer>Raccoon Weather V2</AppContainer>;
+  return (
+    <AppContainer>
+      <CurrentWeather />
+    </AppContainer>
+  );
 };
 
 export default App;
